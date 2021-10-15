749632ae2bcef8d83d7646a53709fe26577cc627
Vanshikasharma620 <sharmavanshika620@gmail.com>
2021-10-15 13:05:32 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,19 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+function register() {
+  x.style.left = "-450px";
+  y.style.left = "35px";
+  z.style.left = "85px";
+}
+function login() {
+  x.style.left = "35px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+}
+// var modal = document.getElementById("login-form");
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
